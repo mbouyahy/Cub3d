51fa227040aeca2641c4ef03f9be57a466664d73
Mourad Bouyahyaoui <mbouyahy@c1r8p1.1337.ma>
2023-10-26 13:50:36 +0100

    the fixed the intersection of the player with the walls (events/key_events.c)

--- events/key_events.c
+++ events/key_events.c
@@ -6,7 +6,7 @@
 /*   By: mbouyahy <mbouyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 19:12:24 by mbouyahy          #+#    #+#             */
-/*   Updated: 2023/10/25 20:18:15 by mbouyahy         ###   ########.fr       */
+/*   Updated: 2023/10/26 13:46:43 by mbouyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,42 +23,80 @@ int	ft_render(t_data *data)
 void rotate_line(t_data *data, float angle)
 {
     data->player.r_angle += angle;
+    data->player.r_angle = remainder_angle(data->player.r_angle);
+}
+
+int  check_wall(t_data *data, float x_value, float y_value)
+{
+    int     grid_x;
+    int     grid_y;
+    float   x;
+    float   y;
+  
+    x = data->player.x + x_value;
+    y = data->player.y + y_value;
+    if (x <= data->cub - x_value  || y <= data->cub - y_value ||
+        x >= data->width_size - data->cub || y >= data->height_size - data->cub)
+      return (1);
+    grid_x = (int)(x / data->cub);
+    grid_y = (int)(y / data->cub);
+    if (grid_x >= data->collums - 1 || grid_y >= data->rows - 1)
+      return (1);
+    if (data->map[grid_y][grid_x] == '0' || 
+        ft_strchr(MAP_DIRECTIONS, data->map[grid_y][grid_x]))
+      return (0);
+    return (1);
+}
+
+void  player_moves(t_data *data, int key, float x_value, float y_value)
+{
+    if (key == A)
+    {
+      if (!check_wall(data, x_value - (M_PI / 2), y_value - (M_PI / 2)))
+      {
+        data->var.move_x += (x_value - (M_PI / 2));
+        data->var.move_y += (y_value - (M_PI / 2));
+      }
+    }
+    if (key == D)
+    {
+      if (!check_wall(data, x_value + (M_PI / 2), y_value + (M_PI / 2)))
+      {
+        data->var.move_x += (x_value + (M_PI / 2));
+        data->var.move_y += (y_value + (M_PI / 2));
+      }
+    }
+    if (key == W)
+    {
+      if (!check_wall(data, x_value, y_value))
+      {
+        data->var.move_x += x_value;
+        data->var.move_y += y_value;
+      }
+    }
+    if (key == S)
+    {
+      if (!check_wall(data, -x_value, -y_value))
+      {
+        data->var.move_x -= x_value;
+        data->var.move_y -= y_value;
+      }
+    }
 }
 
 int key_events(int key, t_data *data)
 {
-    if (key == RIGHT)
-    {
-      rotate_line(data,  (5 * (M_PI / 180)));
-      data->player.r_angle = remainder_angle(data->player.r_angle);
-    }
-    if (key == LEFT)
-    {
-      rotate_line(data,  (-5 * (M_PI / 180)));
-      data->player.r_angle = remainder_angle(data->player.r_angle);
-    }
+    float x_value;
+    float y_value;
 
-    //maybe the movement event not working yet
-    if (key == A)
-    {
-      data->var.move_x += (cos((data->player.r_angle))* 3);
-      data->var.move_y += (sin((data->player.r_angle))* 3);
-    }
-    if (key == D)
-    {
-      data->var.move_x += (cos((data->player.r_angle))* 3);
-      data->var.move_y += (sin((data->player.r_angle))* 3);
-    }
-    if (key == W)
-    {
-      data->var.move_x += (cos((data->player.r_angle))* 3);
-      data->var.move_y += (sin((data->player.r_angle))* 3);
-    }
-    if (key == S)
-    {
-      data->var.move_x -= (cos((data->player.r_angle))* 3);
-      data->var.move_y -= (sin((data->player.r_angle))* 3);
-    }
+    x_value = cos((data->player.r_angle));
+    y_value = sin((data->player.r_angle));
+    if (key == RIGHT)
+      rotate_line(data,  (5 * (M_PI / 180)));
+    if (key == LEFT)
+      rotate_line(data,  (-5 * (M_PI / 180)));
+    //maybe the movement events not working yet
+    player_moves(data, key, x_value, y_value);
     if (key == ESC)
 		  destroy_window(data);
     ft_render(data);
